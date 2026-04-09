c701b21dff7b0dd9f69a1cfe77efb9b21eaa646a
Fahad13-jpg <mohmmadfahad53408@gmail.com>
2026-04-08 23:06:27 -0700

    Fix: correct requirements.txt content

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from CI/CD Pipeline! Deployed via Docker."
+    return 'Hello from CI/CD Pipeline! Deployed via Docker.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
